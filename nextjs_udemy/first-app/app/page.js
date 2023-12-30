807026f7bf7ef1ae21de79f2b88458e87d6da50b
wukdddang <dnr8874@naver.com>
2023-12-31 01:06:11 +0900

    Chore: 식사 어플리케이션 프로젝트 설정 추가

--- nextjs_udemy/first-app/app/page.js
+++ nextjs_udemy/first-app/app/page.js
@@ -1,16 +1,9 @@
-import Link from "next/link";
-
-import Header from "@/components/header";
-
 export default function Home() {
-  console.log("실행중....");
   return (
     <main>
-      <Header />
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p>
-        <Link href="/about">About Us</Link>
-      </p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
