c3d771728a0dade0261940216ad3b7bf8517bd9a
wukdddang <dnr8874@naver.com>
2023-12-30 23:28:28 +0900

    Feat: udemy 강의 학습 - 46강까지

--- nextjs_udemy/react-crash-course/frontend/src/components/PostsList.jsx
+++ nextjs_udemy/react-crash-course/frontend/src/components/PostsList.jsx
@@ -5,25 +5,17 @@ import { useLoaderData } from "react-router-dom";
 function PostsList() {
   const posts = useLoaderData();
 
-  function addPostHandler(postData) {
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setPosts((prevPosts) => {
-      return [...prevPosts, postData];
-    });
-  }
-
   return (
     <>
       {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
-            <Post key={post.id} body={post.body} author={post.author} />
+            <Post
+              key={post.id}
+              id={post.id}
+              body={post.body}
+              author={post.author}
+            />
           ))}
         </ul>
       )}
--- nextjs_udemy/react-crash-course/frontend/src/main.jsx
+++ nextjs_udemy/react-crash-course/frontend/src/main.jsx
@@ -4,7 +4,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Posts, { loader as postsLoader } from "./routes/Posts";
 import "./index.css";
-import NewPost from "./routes/NewPost";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 import RootLayout from "./routes/RootLayout";
 
 const router = createBrowserRouter([
@@ -20,6 +21,12 @@ const router = createBrowserRouter([
           {
             path: "/create-post",
             element: <NewPost />,
+            action: newPostAction,
+          },
+          {
+            path: "/:id",
+            element: <PostDetails />,
+            loader: postDetailsLoader,
           },
         ],
       },
