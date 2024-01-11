3bfd2e071ab1e3a6eaab3030351be0d8bff6f85f
wukdddang <dnr8874@naver.com>
2024-01-12 01:06:20 +0900

    Feat: udemy 강의 학습 - 51 / 192

--- nextjs_udemy_complete_guide/02/src/actions/index.ts
+++ nextjs_udemy_complete_guide/02/src/actions/index.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { db } from "@/db";
 
@@ -9,11 +10,63 @@ export async function editSnippet(id: number, code: string) {
     data: { code },
   });
 
+  revalidatePath(`/snippets/${id}`);
   redirect(`/snippets/${id}`);
 }
 
 export async function deleteSnippet(id: number) {
   await db.snippet.delete({ where: { id } });
 
+  revalidatePath(`/`);
   redirect(`/`);
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // This needs to be a server action.
+    // "use server"; // 위에 use server가 있으니 이제 필요없다.
+
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    // throw new Error("Failed to save to database.");
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong",
+      };
+    }
+  }
+
+  revalidatePath("/");
+  // Redirect the user to the root page
+  redirect("/"); // nextjs에서 redirect는 특별한 에러를 발생시킨다. 그래서 try 문 안에 적으면 안됨.
 }
