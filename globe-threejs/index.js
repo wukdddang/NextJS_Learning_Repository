fa590d7231ad7eb98ddf3e06047a2e3782e48abd
wukdddang <dnr8874@naver.com>
2024-01-18 01:02:31 +0900

    Feat: Three.js 트랜지션 적용 가능 - 추가 공부 필요

--- globe-threejs/index.js
+++ globe-threejs/index.js
@@ -4,6 +4,14 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import getStarfield from "./src/getStarfield.js";
 import { getFresnelMat } from "./src/getFresnelMat.js";
 
+let earthMesh, lightsMesh, cloudsMesh, glowMesh, stars; // 전역 스코프 변수 선언
+
+function loadTexture(loader, url) {
+  return new Promise((resolve, reject) => {
+    loader.load(url, resolve, undefined, reject);
+  });
+}
+
 function init() {
   const rotationSpeed = 0.002; // 회전 속도 조절
 
@@ -19,10 +27,9 @@ function init() {
   const earthGroup = new THREE.Group();
   earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
   earthGroup.position.x += 2;
-
   scene.add(earthGroup);
-  const controls = new OrbitControls(camera, renderer.domElement);
 
+  const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableZoom = false;
   controls.enablePan = false;
   controls.enableDamping = true;
@@ -30,59 +37,96 @@ function init() {
 
   const detail = 12;
   const loader = new THREE.TextureLoader();
-  const geometry = new THREE.IcosahedronGeometry(1, detail);
-  const material = new THREE.MeshPhongMaterial({
-    map: loader.load("./textures/8081_earthmap10k.jpg"),
-    specularMap: loader.load("./textures/8081_earthspec10k.jpg"),
-    bumpMap: loader.load("./textures/8081_earthbump10k.jpg"),
-    bumpScale: 0.04,
-  });
-  const earthMesh = new THREE.Mesh(geometry, material);
-  // earthMesh.rotation.y = THREE.Math.degToRad(-100); // 서경 100도 위치로 초기 회전 설정
-  earthGroup.add(earthMesh);
 
-  const lightsMat = new THREE.MeshBasicMaterial({
-    map: loader.load("./textures/8081_earthlights10k.jpg"),
-    blending: THREE.AdditiveBlending,
-  });
-  const lightsMesh = new THREE.Mesh(geometry, lightsMat);
-  earthGroup.add(lightsMesh);
+  Promise.all([
+    loadTexture(loader, "./textures/8081_earthmap10k.jpg"),
+    loadTexture(loader, "./textures/8081_earthspec10k.jpg"),
+    loadTexture(loader, "./textures/8081_earthbump10k.jpg"),
+    loadTexture(loader, "./textures/8081_earthlights10k.jpg"),
+    loadTexture(loader, "./textures/04_earthcloudmap.jpg"),
+    loadTexture(loader, "./textures/05_earthcloudmaptrans.jpg"),
+    // 다른 텍스처 로드 프로미스 추가...
+  ]).then(
+    ([earthMap, earthSpec, earthBump, earthLights, cloudMap, cloudTrans]) => {
+      const geometry = new THREE.IcosahedronGeometry(1, detail);
 
-  const cloudsMat = new THREE.MeshStandardMaterial({
-    map: loader.load("./textures/04_earthcloudmap.jpg"),
-    transparent: true,
-    opacity: 0.8,
-    blending: THREE.AdditiveBlending,
-    alphaMap: loader.load("./textures/05_earthcloudmaptrans.jpg"),
-    // alphaTest: 0.3,
-  });
-  const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
-  cloudsMesh.scale.setScalar(1.003);
-  earthGroup.add(cloudsMesh);
+      const material = new THREE.MeshPhongMaterial({
+        map: earthMap,
+        specularMap: earthSpec,
+        bumpMap: earthBump,
+        bumpScale: 0.04,
+      });
+      earthMesh = new THREE.Mesh(geometry, material);
+      earthGroup.add(earthMesh);
 
-  const fresnelMat = getFresnelMat();
-  const glowMesh = new THREE.Mesh(geometry, fresnelMat);
-  glowMesh.scale.setScalar(1.0);
-  earthGroup.add(glowMesh);
+      const lightsMat = new THREE.MeshBasicMaterial({
+        map: earthLights,
+        blending: THREE.AdditiveBlending,
+      });
+      lightsMesh = new THREE.Mesh(geometry, lightsMat);
+      earthGroup.add(lightsMesh);
 
-  const stars = getStarfield({ numStars: 2000 });
-  scene.add(stars);
+      const cloudsMat = new THREE.MeshStandardMaterial({
+        map: cloudMap,
+        transparent: true,
+        opacity: 0.8,
+        blending: THREE.AdditiveBlending,
+        alphaMap: cloudTrans,
+      });
+      cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+      cloudsMesh.scale.setScalar(1.003);
+      earthGroup.add(cloudsMesh);
 
-  const sunLight = new THREE.DirectionalLight(0xffffff);
-  sunLight.position.set(-100, 100, -130);
-  scene.add(sunLight);
+      const fresnelMat = getFresnelMat();
+
+      glowMesh = new THREE.Mesh(geometry, fresnelMat);
+      glowMesh.scale.setScalar(1.0);
+      earthGroup.add(glowMesh);
+
+      stars = getStarfield({ numStars: 2000 });
+      scene.add(stars);
+
+      const sunLight = new THREE.DirectionalLight(0xffffff);
+      sunLight.position.set(-100, 100, -130);
+      scene.add(sunLight);
+
+      // 모든 것이 로드된 후 animate 호출
+      animate();
+      onEverythingLoaded();
+    }
+  );
 
   function animate() {
     requestAnimationFrame(animate);
 
+    if (earthMesh) {
+      earthMesh.rotation.y += rotationSpeed;
+    }
+
+    if (lightsMesh) {
+      lightsMesh.rotation.y += rotationSpeed;
+    }
+
+    if (cloudsMesh) {
+      cloudsMesh.rotation.y += rotationSpeed;
+    }
+
+    if (glowMesh) {
+      glowMesh.rotation.y += rotationSpeed;
+    }
+
+    if (stars) {
+      stars.rotation.y -= 0.0002;
+    }
+
     // 지구 회전 조절
-    earthMesh.rotation.y += rotationSpeed;
-    lightsMesh.rotation.y += rotationSpeed;
-    cloudsMesh.rotation.y += rotationSpeed;
-    glowMesh.rotation.y += rotationSpeed;
+    // earthMesh.rotation.y += rotationSpeed;
+    // lightsMesh.rotation.y += rotationSpeed;
+    // cloudsMesh.rotation.y += rotationSpeed;
+    // glowMesh.rotation.y += rotationSpeed;
 
     // 기타 애니메이션 코드
-    stars.rotation.y -= 0.0002;
+    // stars.rotation.y -= 0.0002;
     renderer.render(scene, camera);
   }
 
@@ -90,10 +134,11 @@ function init() {
     const event = new Event("everythingLoaded");
     window.dispatchEvent(event);
     console.log("everything loaded");
+    const canvas = document.getElementsByTagName("canvas")[0];
+    if (canvas) {
+      canvas.style.opacity = 1;
+    }
   }
-
-  animate();
-  onEverythingLoaded();
 }
 
 function handleWindowResize() {
--- nextjs_templates/app/page.tsx
+++ nextjs_templates/app/page.tsx
@@ -3,30 +3,38 @@
 import Script from "next/script";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [loaded, setLoaded] = useState(false);
+  const [opacity, setOpacity] = useState(0);
 
   useEffect(() => {
-    const handleBundleLoaded = () => {
-      setIsLoading(false);
+    const handleEverythingLoaded = () => {
+      setOpacity(1);
+      setLoaded(true);
     };
 
-    window.addEventListener("bundleLoaded", handleBundleLoaded);
+    window.addEventListener("everythingLoaded", handleEverythingLoaded);
 
     return () => {
-      window.removeEventListener("bundleLoaded", handleBundleLoaded);
+      window.removeEventListener("everythingLoaded", handleEverythingLoaded);
     };
   }, []);
 
   return (
     <>
-      {!isLoading ? (
-        <div className="absolute bg-white w-[300px] h-[300px] top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] rounded-lg border-purple-600 border-4">
-          <Link href="/good">Google Login</Link>
-        </div>
+      {!loaded ? (
+        <div className="loader bg-white">Loading...</div>
       ) : (
-        <div className="loader">Loading...</div> // 여기에 커스텀 로더를 넣으세요.
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+          className="absolute bg-white w-[300px] h-[300px] top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] rounded-lg border-purple-600 border-4"
+        >
+          <Link href="/good">Google Login</Link>
+        </motion.div>
       )}
       <Script src="/bundle.js" />
     </>
