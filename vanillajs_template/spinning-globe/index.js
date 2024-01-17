46334420064665007b9272fa647cad8f8aad150d
wukdddang <dnr8874@naver.com>
2024-01-17 15:49:48 +0900

    Feat: Three.js 사용한 globe 제작 템플릿

---
+++ vanillajs_template/spinning-globe/index.js
@@ -0,0 +1,94 @@
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+
+import getStarfield from "./src/getStarfield.js";
+import { getFresnelMat } from "./src/getFresnelMat.js";
+
+const rotationSpeed = 0.002; // 회전 속도 조절
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
+earthGroup.position.x += 2;
+
+scene.add(earthGroup);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+controls.enableZoom = false;
+controls.enablePan = false;
+controls.enableDamping = true;
+controls.rotateSpeed = 0.3;
+
+const detail = 12;
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, detail);
+const material = new THREE.MeshPhongMaterial({
+  map: loader.load("./textures/00_earthmap1k.jpg"),
+  specularMap: loader.load("./textures/02_earthspec1k.jpg"),
+  bumpMap: loader.load("./textures/01_earthbump1k.jpg"),
+  bumpScale: 0.04,
+});
+const earthMesh = new THREE.Mesh(geometry, material);
+earthGroup.add(earthMesh);
+
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("./textures/03_earthlights1k.jpg"),
+  blending: THREE.AdditiveBlending,
+});
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightsMesh);
+
+const cloudsMat = new THREE.MeshStandardMaterial({
+  map: loader.load("./textures/04_earthcloudmap.jpg"),
+  transparent: true,
+  opacity: 0.8,
+  blending: THREE.AdditiveBlending,
+  alphaMap: loader.load("./textures/05_earthcloudmaptrans.jpg"),
+  // alphaTest: 0.3,
+});
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+cloudsMesh.scale.setScalar(1.003);
+earthGroup.add(cloudsMesh);
+
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+glowMesh.scale.setScalar(1.0);
+earthGroup.add(glowMesh);
+
+const stars = getStarfield({ numStars: 2000 });
+scene.add(stars);
+
+const sunLight = new THREE.DirectionalLight(0xffffff);
+sunLight.position.set(-100, 100, -130);
+scene.add(sunLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // 지구 회전 조절
+  earthMesh.rotation.y += rotationSpeed;
+  lightsMesh.rotation.y += rotationSpeed;
+  cloudsMesh.rotation.y += rotationSpeed;
+  glowMesh.rotation.y += rotationSpeed;
+
+  // 기타 애니메이션 코드
+  stars.rotation.y -= 0.0002;
+  renderer.render(scene, camera);
+}
+
+animate();
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", handleWindowResize, false);
