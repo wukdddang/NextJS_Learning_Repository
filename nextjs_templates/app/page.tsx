77fef8036b7c7f3b75beadeb1596d396257358a5
wukdddang <dnr8874@naver.com>
2024-01-17 15:48:56 +0900

    Feat: Nextui 사용 예시

--- nextjs_templates/app/page.tsx
+++ nextjs_templates/app/page.tsx
@@ -1,9 +1,10 @@
 import Calendar from "@/components/Calendar";
+import CustomCheckbox from "@/components/custom-checkbox";
 
 export default function Home() {
   return (
     <>
-      <Calendar />
+      <CustomCheckbox />
     </>
   );
 }
